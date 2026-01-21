4d1e6c65061f72730f56424c68a7ed6920de8c0b
WalidurTanjim <mohammadtanjim92@gmail.com>
2026-01-21 22:26:02 +0600

    L2B6M9 first commit

---
+++ file1.js
@@ -0,0 +1,12 @@
+const { a } = require('./file2');
+const { a: b } = require('./file3');
+
+// const { add } = require('./utils/add');
+// const { subs } = require('./utils/subs');
+
+const { add, subs } = require('./utils');
+
+// console.log(a, b);
+
+console.log(add(a, b));
+console.log(subs(b, a));
---
+++ utils/subs.js
@@ -0,0 +1,5 @@
+const subs = (a, b) => {
+    return a - b;
+}
+
+module.exports = { subs };
